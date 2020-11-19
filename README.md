658c66866425afe3233b0010cccce60f18d8bb4d
lowewenzel <lowewenzel@gmail.com>
2020-11-19 01:31:39 -0800

    Initialize video-cutter

---
+++ README.md
@@ -0,0 +1,64 @@
+# Video Timestamps Cutter CLI
+
+Command line interface for cutting one video into multiple parts utilizing `moviepy`, via a CSV.
+
+## Installation
+
+Either download this as a zip, or clone the repo. Utilize a virtual env (I used 3.7 to develop).
+
+```bash
+git clone https://github.com/lowewenzel/video-cutter.git
+
+cd video-cutter
+
+# virtual env
+python3 -m venv env
+source env/bin/activate
+pip install
+```
+
+## Usage
+
+For simplicity, place two files in the same folder as `main.py`. (You can also use any directory reachable by bash and python).
+
+```bash
+python main.py video_file.mp4 rows.csv
+python main.py "video with spaces.mp4" "csv with spaces.csv"
+```
+
+This will create your video files in the `out/` directory.
+
+#### Video File
+
+Format can be any format supported by [moviepy](https://zulko.github.io/moviepy/)(which is a lot).
+
+#### CSV file
+
+This is where the parts are separated/dictated. Use the following CSV format:
+
+###### As a CSV
+
+```csv
+File Title,Start Time,End Time
+Intro,0:00:02,0:01:16
+Chapter 1,0:01:17,0:24:35
+Chapter 6-10,0:24:37,0:34:41
+Credits,0:34:46,0:36:04
+```
+
+###### As a table
+
+| File Title   | Start Time | End Time |
+| ------------ | ---------- | -------- |
+| Intro        | 0:00:02    | 0:01:16  |
+| Chapter 1    | 0:01:17    | 0:24:35  |
+| Chapter 6-10 | 0:24:37    | 0:34:41  |
+| Credits      | 0:34:46    | 0:36:04  |
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ main.py
@@ -0,0 +1,54 @@
+from moviepy.editor import *
+from pytimeparse.timeparse import timeparse
+import csv
+
+
+def render_video_sections(file_name, video_sections):
+    video_file = VideoFileClip(file_name)
+
+    for section in video_sections:
+        start_time = timeparse(section['start_time'])
+        end_time = timeparse(section['end_time']) + 1
+        title = "out/" + section['title'] + ".mp4"
+
+        clip = video_file.subclip(start_time, end_time)
+
+        clip.write_videofile(title,
+                             audio_codec='aac',
+                             temp_audiofile='temp-audio.m4a',
+                             remove_temp=True
+                             )
+
+
+def read_csv_to_video_sections(file_name):
+    with open(file_name, newline='') as csv_file:
+        rows = csv.reader(csv_file)
+        # skip first line
+        next(rows)
+
+        video_sections = []
+        for row in rows:
+            section = {
+                'title': row[0],
+                'start_time': row[1],
+                'end_time': row[2],
+            }
+            video_sections.append(section)
+
+        return video_sections
+
+
+if __name__ == "__main__":
+    if len(sys.argv) is not 3:
+        print("Err: Missing file inputs! \n\n--Proper Usage--")
+        print('python main.py video_file.mp4 rows.csv')
+        print(
+            'python main.py "video with spaces.mp4" "csv with spaces.csv"\n----------------')
+        exit()
+
+    # File names
+    video_file = sys.argv[1]
+    csv_file = sys.argv[2]
+
+    video_sections = read_csv_to_video_sections(csv_file)
+    render_video_sections(video_file, video_sections)
